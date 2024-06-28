7c1ad08c03ec350538d4a51ded6b50e7011c0a0c
Mayank <innovativecreations195@gmail.com>
2024-06-28 06:10:29 +0530

    not able to print the variables

--- rpc.py
+++ rpc.py
@@ -1,12 +1,11 @@
-from flask import Flask # type: ignore
+from flask import Flask, render_template_string # type: ignore
 import random
 
 random_number = random.randint(0, 9)
 print(random_number)
 
 app = Flask(__name__)
-comp =""
-clicked = 0
+
 
 @app.route('/')
 def home():
@@ -19,20 +18,26 @@ def home():
 
 @app.route("/<guess>")
 def get_Data(guess):
-    
+
     comp = random.choice(["s", "r", "p"])
-    clicked += 1
-    print(comp)
-    print(clicked)
+    result =""
     if guess == "s" and comp == "p":
-        return "<h1>You won</h1>"
+        return render_template_string('''<h1>You won</h1> 
+        <p>you chose: {{ guess }}</p>
+         <p>comp chose: {{ comp }}</p>''')
     elif guess == "p" and comp == "r":
-        return "<h1>You won</h1>"
+        return render_template_string('''<h1>You won</h1> 
+        <p>you chose: {{ guess }}</p>
+         <p>comp chose: {{ comp }}</p>''')
     elif guess == "r" and comp == "s":
-        return "<h1>You won</h1>"
+        return render_template_string('''<h1>You won</h1> 
+        <p>you chose: {{ guess }}</p>
+         <p>comp chose: {{ comp }}</p>''')
+    elif guess == comp:
+        return "TIE"
     else:
-        return "<h1>Better luck next time</h1> <br>"\
-               "<img src='https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDlya3Vzd2NnYWdpN2JmNnhxcHExempqbzl3MDgzYmt6NXRvem51byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jnQYWZ0T4mkhCmkzcn/giphy.webp'>"
+        return render_template_string("""<h1>Better luck next time</h1> <p>you chose: {{guess}}</p> <p>comp chose: {comp}</p> <br>
+               "<img src='https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDlya3Vzd2NnYWdpN2JmNnhxcHExempqbzl3MDgzYmt6NXRvem51byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jnQYWZ0T4mkhCmkzcn/giphy.webp'>""")
 
 if __name__ == "__main__":
     app.run(debug=True)
